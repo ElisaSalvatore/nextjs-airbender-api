7ea869e7ec52328e65c1728574248f0a86550296
ElisaSalvatore <elisa.salvatore110@gmail.com>
2022-09-21 16:35:03 +0200

    creates dynamic url path using getStaticPaths and passing params

---
+++ pages/characters/[characterId].js
@@ -0,0 +1,36 @@
+export default function Character( {character} ) {
+    return (
+        <>
+            <img src={character.photoUrl} alt={character.name}/>
+            <h1>{character.name}</h1>
+            <p>Affiliation: {character.affiliation}</p>
+        </>
+    )
+}
+
+export async function getStaticProps( {params} ) {
+    //return an array, that's why naming "results" variable
+    const results = await fetch(`https://last-airbender-api.herokuapp.com/api/v1/characters?name=${params.characterId}`).then(res => res.json());
+
+    return {
+        props: {
+            character: results[0],
+        }
+    }
+}
+
+export async function getStaticPaths() {
+    const characters = await fetch('https://last-airbender-api.herokuapp.com/api/v1/characters?perPage=500').then(res => res.json());
+
+    return {
+        paths: characters.map(character => {
+            const characterId = character.name.toLowerCase();
+            return {
+                params: {
+                    characterId
+                }
+            }
+        }),
+        fallback: false, //if the page is not found we want display an 404 not found page 
+    }
+}
